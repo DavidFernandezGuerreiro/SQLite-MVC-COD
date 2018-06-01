c02480ae8c5e04b00f92fe9a120d445ce3bee539
David <David@192.168.1.16>
2018-06-01 09:45:30 +0100

    Main. Instanciado el objeto de la clase "DatosTabla" y lo hacemos visible. Package sqliteapp. Class SQLiteApp.

---
+++ src/sqliteapp/SQLiteApp.java
@@ -0,0 +1,22 @@
+
+package sqliteapp;
+
+import Vista.DatosTabla;
+
+/**
+ *
+ * @author David
+ */
+public class SQLiteApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        DatosTabla obx=new DatosTabla();
+        obx.setVisible(true);
+        
+    }
+    
+}
